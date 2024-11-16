8f5536880b189aef486298934decaeba0e208eb9
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-11-16 19:11:12 +0700

    feat: add card component

---
+++ components/Card.tsx
@@ -0,0 +1,44 @@
+import { SampahTypes } from "@/types/Sampah";
+import { useCurrencyFormatter } from "@/utils/useCurrencyFormatter";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  // Link as LinkNextUI,
+} from "@nextui-org/react";
+
+function CardComponent(props: SampahTypes) {
+  const { formatRupiah } = useCurrencyFormatter();
+
+  return (
+    <Card
+      shadow="lg"
+      // as={LinkNextUI}
+      // href={`${props.id}`}
+      // isPressable
+      className="rounded-lg bg-[#01683B] py-2"
+    >
+      <CardBody className="overflow-visible">
+        <Image
+          shadow="sm"
+          radius="md"
+          width="100%"
+          alt={props.nama_sampah}
+          className="h-[140px] w-full object-cover"
+          src={"https://placehold.co/600x400/EEE/31343C"}
+        />
+      </CardBody>
+      <CardFooter className="py-1">
+        <p className="text-lg font-bold text-white">{props.nama_sampah}</p>
+      </CardFooter>
+      <CardFooter className="py-1">
+        <p className="text-md text-white">
+          {formatRupiah(props.harga_sekarang)} / {props.nama_suk}
+        </p>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default CardComponent;
