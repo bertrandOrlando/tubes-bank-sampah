c39b33300002784925afee84a65096598188f1c8
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-11-17 19:25:59 +0700

    add: member transaction history page

---
+++ app/riwayat-transaksi/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import React, { useState } from "react";
+import { parseDate, getLocalTimeZone } from "@internationalized/date";
+import { useDateFormatter as DateCalendarFormatter } from "@react-aria/i18n";
+import TransaksiData from "@/data/Transaksi.json";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  DatePicker,
+  Button,
+  Checkbox,
+} from "@nextui-org/react";
+import { useCurrencyFormatter } from "@/utils/useCurrencyFormatter";
+import { useDateFormatter } from "@/utils/useDateFormatter";
+import ModalComponent from "@/components/Modal";
+
+const RiwayatTransaksi = () => {
+  // Logic Calendar
+  const today = new Date();
+  const currDate = today.toISOString().slice(0, 10);
+
+  const [startDate, setStartDate] = useState(parseDate(currDate));
+  const [endDate, setEndDate] = useState(parseDate(currDate));
+
+  //   Logic Filter Transaksi
+  const [filterStart, setFilterStart] = useState<boolean>(false);
+  const [filterEnd, setFilterEnd] = useState<boolean>(false);
+  const filterHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("filter transaction with start date and end date");
+  };
+
+  //   Logic Table Transaksi
+  const data = TransaksiData.map((item, index) => {
+    return { ...item, key: index + 1 };
+  });
+  const { formatRupiah } = useCurrencyFormatter();
+  const { formatDate } = useDateFormatter();
+
+  const columns = [
+    {
+      key: "key",
+      label: "NO",
+    },
+    {
+      key: "tanggal_masuk",
+      label: "TANGGAL",
+    },
+    {
+      key: "total_transaksi",
+      label: "TOTAL PENDAPATAN",
+    },
+    {
+      key: "",
+      label: "",
+    },
+  ];
+
+  return (
+    <div className="p-8 flex flex-col items-center justify-center gap-y-6">
+      <h1 className="font-bold text-3xl my-4 ">Riwayat Transaksi</h1>
+      <div className="flex flex-row justify-evenly gap-12 ">
+        <div className="flex flex-col justify-between items-center">
+          <Checkbox isSelected={filterStart} onValueChange={setFilterStart}>
+            Filter awal transaksi
+          </Checkbox>
+          <DatePicker
+            className="max-w-[284px]"
+            label="Tanggal Mulai Transaksi"
+            value={startDate}
+            onChange={setStartDate}
+            isDisabled={!filterStart}
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-5">
+          <Checkbox isSelected={filterEnd} onValueChange={setFilterEnd}>
+            Filter akhir transaksi
+          </Checkbox>
+          <DatePicker
+            className="max-w-[284px]"
+            label="Tanggal Berakhir Transaksi"
+            value={endDate}
+            onChange={setEndDate}
+            isDisabled={!filterEnd}
+          />
+        </div>
+      </div>
+      <Button
+        color="primary"
+        variant="ghost"
+        className=""
+        onClick={filterHandler}
+      >
+        Lihat Transaksi
+      </Button>
+      <div className="w-3/5">
+        <Table isStriped aria-label="Seluruh transaksi pengguna">
+          <TableHeader columns={columns}>
+            {(column) => (
+              <TableColumn key={column.key} className="text-center">
+                {column.label}{" "}
+              </TableColumn>
+            )}
+          </TableHeader>
+          <TableBody
+            items={data}
+            emptyContent={"Anda belum memiliki transaksi saat ini"}
+          >
+            {(item) => (
+              <TableRow key={item.transaksi_id} className="">
+                <TableCell className="text-center font-semibold">
+                  {item.key}
+                </TableCell>
+                <TableCell className="text-center">
+                  {formatDate(item.tanggal_masuk)}
+                </TableCell>
+                <TableCell className="text-center">
+                  {formatRupiah(item.total_transaksi)}
+                </TableCell>
+                <TableCell className="flex items-center justify-center">
+                  <ModalComponent {...item} />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+RiwayatTransaksi.getLayout = () => null;
+
+export default RiwayatTransaksi;
---
+++ components/Modal.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  useDisclosure,
+  Button,
+} from "@nextui-org/react";
+
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
+
+import { TransaksiSampahTypes, TransaksiTypes } from "@/types/Transaksi";
+import { useCurrencyFormatter } from "@/utils/useCurrencyFormatter";
+
+const ModalComponent = (props: TransaksiTypes) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { formatRupiah } = useCurrencyFormatter();
+
+  const itemSampah = props.item_sampah.map(
+    (item: TransaksiSampahTypes, index: number) => {
+      return {
+        ...item,
+        key: index + 1,
+        harga_sampah: formatRupiah(item.harga_sampah) + "/" + item.nama_suk,
+      };
+    }
+  );
+
+  const columns = [
+    {
+      key: "key",
+      label: "NO",
+    },
+    {
+      key: "nama_sampah",
+      label: "NAMA",
+    },
+    {
+      key: "jumlah_sampah",
+      label: "KUANTITAS",
+    },
+    {
+      key: "harga_sampah",
+      label: "HARGA",
+    },
+    {
+      key: "subtotal",
+      label: "SUBTOTAL",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Button color="primary" variant="ghost" className="" onPress={onOpen}>
+        Lihat Detail
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        scrollBehavior={"inside"}
+        size="3xl"
+        backdrop="blur"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 border-b-3">
+                Transaksi tanggal {props.tanggal_masuk}
+              </ModalHeader>
+              <ModalBody className="p-10">
+                <h1 className="font-bold text-lg">Detail Penjualan Sampah</h1>
+                <h1 className="font-semibold text-md">
+                  Total Pendapatan : {formatRupiah(props.total_transaksi)}
+                </h1>
+
+                <Table isStriped aria-label="detail transaksi sampah">
+                  <TableHeader columns={columns}>
+                    {(column) => (
+                      <TableColumn key={column.key}>{column.label}</TableColumn>
+                    )}
+                  </TableHeader>
+                  <TableBody
+                    items={itemSampah}
+                    emptyContent={
+                      "Tidak terdapat detail informasi untuk transaksi ini"
+                    }
+                  >
+                    {(item) => (
+                      <TableRow key={item.key}>
+                        {(columnKey) => (
+                          <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                        )}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+export default ModalComponent;
