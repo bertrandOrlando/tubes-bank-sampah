3b8478a7e44ff5e850d3657eb123c65709411ecc
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-11-23 22:42:06 +0700

    feat: add breadcrumb on edit member page

--- app/admin/data-member/edit/[pengguna_id]/page.tsx
+++ app/admin/data-member/edit/[pengguna_id]/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React from "react";
-import { Input, Card, CardBody, Button } from "@nextui-org/react";
+import {
+  Input,
+  Card,
+  CardBody,
+  Button,
+  BreadcrumbItem,
+  Breadcrumbs,
+} from "@nextui-org/react";
 
 type Params = {
   pengguna_id: number;
@@ -11,71 +18,81 @@ const EditMemberPage = ({ params }: { params: Params }) => {
   console.log(pengguna_id);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-y-6 p-8">
-      <h1 className="my-4 text-3xl font-bold">Edit Data Member</h1>
-      <Card className="grid w-1/4 py-10">
-        <CardBody className="flex items-center">
-          <Input type="text" label="Nama" className="w-72" value={"Asep"}>
-            Nama
-          </Input>
-        </CardBody>
-        <CardBody className="flex items-center">
-          <Input
-            type="text"
-            label="No Telp"
-            className="w-72"
-            value={"089123456789"}
-          >
-            No Telp
-          </Input>
-        </CardBody>
-        <CardBody className="flex items-center">
-          <Input
-            type="text"
-            label="Alamat"
-            className="w-72"
-            value={"Jalan Ciumbuleuit No. 94"}
-          >
-            Alamat
-          </Input>
-        </CardBody>
-        <CardBody className="flex items-center">
-          <Input
-            type="email"
-            label="Email"
-            className="w-72"
-            value={"asep123@gmail.com"}
-          >
-            Email
-          </Input>
-        </CardBody>
-        <CardBody className="flex items-center">
-          <Input
-            type="text"
-            label="Kelurahan"
-            className="w-72"
-            value={"Sukahaji"}
-          >
-            Kelurahan
-          </Input>
-        </CardBody>
-        <CardBody className="flex items-center">
-          <Input
-            type="text"
-            label="Kecamatan"
-            className="w-72"
-            value={"Babakan Ciparay"}
-          >
-            Kecamatan
-          </Input>
-        </CardBody>
-        <CardBody className="flex items-center">
-          <Button color="primary" className="w-24">
-            Simpan
-          </Button>
-        </CardBody>
-      </Card>
-    </div>
+    <>
+      <Breadcrumbs variant="bordered" className="ml-4 mt-4">
+        <BreadcrumbItem href="/">Home</BreadcrumbItem>
+        <BreadcrumbItem href="/admin">Admin</BreadcrumbItem>
+        <BreadcrumbItem href="/admin/data-member">Data Member</BreadcrumbItem>
+        <BreadcrumbItem href="/admin/data-member" className="font-bold">
+          Edit Pengguna
+        </BreadcrumbItem>
+      </Breadcrumbs>
+      <div className="flex flex-col items-center justify-center gap-y-6 p-8">
+        <h1 className="my-4 text-3xl font-bold">Edit Data Member</h1>
+        <Card className="grid w-1/4 py-10">
+          <CardBody className="flex items-center">
+            <Input type="text" label="Nama" className="w-72" value={"Asep"}>
+              Nama
+            </Input>
+          </CardBody>
+          <CardBody className="flex items-center">
+            <Input
+              type="text"
+              label="No Telp"
+              className="w-72"
+              value={"089123456789"}
+            >
+              No Telp
+            </Input>
+          </CardBody>
+          <CardBody className="flex items-center">
+            <Input
+              type="text"
+              label="Alamat"
+              className="w-72"
+              value={"Jalan Ciumbuleuit No. 94"}
+            >
+              Alamat
+            </Input>
+          </CardBody>
+          <CardBody className="flex items-center">
+            <Input
+              type="email"
+              label="Email"
+              className="w-72"
+              value={"asep123@gmail.com"}
+            >
+              Email
+            </Input>
+          </CardBody>
+          <CardBody className="flex items-center">
+            <Input
+              type="text"
+              label="Kelurahan"
+              className="w-72"
+              value={"Sukahaji"}
+            >
+              Kelurahan
+            </Input>
+          </CardBody>
+          <CardBody className="flex items-center">
+            <Input
+              type="text"
+              label="Kecamatan"
+              className="w-72"
+              value={"Babakan Ciparay"}
+            >
+              Kecamatan
+            </Input>
+          </CardBody>
+          <CardBody className="flex items-center">
+            <Button color="primary" className="w-24">
+              Simpan
+            </Button>
+          </CardBody>
+        </Card>
+      </div>
+    </>
   );
 };
 
