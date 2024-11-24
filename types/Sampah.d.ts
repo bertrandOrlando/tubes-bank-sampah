e89f79eda6b334edcc545d1c650ee6cc526217c9
christianHadinata <christianhadinata2004@gmail.com>
2024-11-24 18:24:47 +0700

    fix sampah data and sampah types

--- types/Sampah.d.ts
+++ types/Sampah.d.ts
@@ -2,7 +2,6 @@ export type SampahTypes = {
   sampah_id: number;
   nama_sampah: string;
   harga_sekarang: number;
-  jenis_sampah: string;
   nama_suk: string;
   slug_image: string;
   kuantitas: number;
