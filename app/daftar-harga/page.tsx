d2c243731ffcf0a711152acaecc82f078ee928bb
christianHadinata <christianhadinata2004@gmail.com>
2024-12-02 22:31:12 +0700

    feat: get data from server

--- app/daftar-harga/page.tsx
+++ app/daftar-harga/page.tsx
@@ -1,22 +1,32 @@
 "use client";
 
-import React, { useState } from "react";
-import hargaSampah from "@/data/sampah.json";
+import React, { useEffect, useState } from "react";
 import { SampahTypes } from "@/types/Sampah";
 import Card from "@/components/Card";
 import { Input } from "@nextui-org/react";
+import axios from "axios";
 
 const DaftarHarga = () => {
-  const [dataSampah, setDataSampah] = useState<SampahTypes[]>(hargaSampah);
+  const [dataSampah, setDataSampah] = useState<SampahTypes[]>();
   const [searchValue, setSearchValue] = useState<string>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("http://localhost:5000/api/sampah/");
+
+      setDataSampah(data);
+    };
+
+    fetchData();
+  }, []);
 
   const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setSearchValue(value);
     setDataSampah(() =>
-      hargaSampah.filter((e: SampahTypes) =>
-        e.nama_sampah.toLowerCase().includes(value.toLowerCase())
-      )
+      dataSampah?.filter((e: SampahTypes) =>
+        e.nama_sampah.toLowerCase().includes(value.toLowerCase()),
+      ),
     );
   };
 
@@ -31,7 +41,7 @@ const DaftarHarga = () => {
         onChange={searchHandler}
       />
       <div className="container flex max-w-6xl cursor-default flex-row flex-wrap justify-evenly gap-6 rounded-md p-5">
-        {dataSampah.map((dataSampah: SampahTypes, index: number) => {
+        {dataSampah?.map((dataSampah: SampahTypes, index: number) => {
           return <Card key={index} {...dataSampah} />;
         })}
       </div>
